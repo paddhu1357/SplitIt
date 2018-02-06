0f6971be953b8bfc99941602b7a8afe38d4332e1
padmabhushan <padmabhushan1287@gmail.com>
2018-02-06 23:37:56 +0530

    for resolution of CR 36034 by Bhushan

--- GitTest/src/com/glx/gittest/GitTest.java
+++ GitTest/src/com/glx/gittest/GitTest.java
@@ -5,6 +5,18 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("this is rajasekhar");
 		System.out.println("this is somathing new");
-		System.out.println("this line is added by eswar9052 by eswar");
+		System.out.println("this line is added by eswar9052 by eswar rahjasekhadsfsjf");
+	}
+	public void f1()
+	{
+		System.out.println("rajasekhar");
+	}
+	public void f2()
+	{
+		System.out.println("eswar");
+	}
+	public void f3()
+	{
+		System.out.println("bhushan");
 	}
 }
